bb9ad97eee34dd3c5584fb700caeb191be1de7f1
DCD <nickhowell2@gmail.com>
2025-04-08 09:42:17 -0700

    Updated CloudFlare worker config to host SwaggerUI

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,16 +1,15 @@
-addEventListener('fetch', (event) => {
+addEventListener('fetch', (event: FetchEvent) => {
     event.respondWith(handleRequest(event.request));
-  });
-  
-  async function handleRequest(request: Request): Promise<Response> {
-      const url = new URL(request.url);
-      if (url.pathname === "/") {
-          // Adjusted to reference the correct location of index.html
-          const html = await fetch("index.html");  // This assumes it's in the root of the worker's assets
-          return new Response(html.body, {
-              headers: { "Content-Type": "text/html" }
-          });
-      }
-      return new Response("Not Found", { status: 404 });
-  }
-  
+});
+
+async function handleRequest(request: Request): Promise<Response> {
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+        // Adjusted to reference the correct location of index.html
+        const html = await fetch("index.html");  // This assumes it's in the root of the worker's assets
+        return new Response(html.body, {
+            headers: { "Content-Type": "text/html" }
+        });
+    }
+    return new Response("Not Found", { status: 404 });
+}
